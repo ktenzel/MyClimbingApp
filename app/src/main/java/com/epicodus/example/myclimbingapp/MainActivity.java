4b870d1a228828bc8c4b4074586135a295b14686
kyle tenzeldam <ktenzel@gmail.com>
2018-03-16 10:07:20 -0700

    add typeface and color to app

--- app/src/main/java/com/epicodus/example/myclimbingapp/MainActivity.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.epicodus.example.myclimbingapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/Base02.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
     }
 }
