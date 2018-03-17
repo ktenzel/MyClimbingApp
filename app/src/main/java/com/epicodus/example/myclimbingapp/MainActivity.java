1d325016ceafb28a6aa1f86905750a8da6d5c541
kyle tenzeldam <ktenzel@gmail.com>
2018-03-16 21:00:41 -0700

    add routes to buttons on mainactivity

--- app/src/main/java/com/epicodus/example/myclimbingapp/MainActivity.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/MainActivity.java
@@ -1,24 +1,55 @@
 package com.epicodus.example.myclimbingapp;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.appNameTextView)
-    TextView mAppNameTextView;
+  @BindView(R.id.routesButton)
+  Button mRoutesButton;
+  @BindView(R.id.aboutButton)
+  Button mAboutButton;
+  @BindView(R.id.contactButton)
+  Button mContactButton;
+  @BindView(R.id.appNameTextView)
+  TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        Typeface baseFont = Typeface.createFromAsset(getAssets(), "fonts/Base 02.ttf");
+//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface baseFont = Typeface.createFromAsset(getAssets(), "fonts/Base02.ttf");
         mAppNameTextView.setTypeface(baseFont);
+
+        mRoutesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, RoutesActivity.class);
+                    startActivity(intent);
+            }
+        });
+        mAboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+        mContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
