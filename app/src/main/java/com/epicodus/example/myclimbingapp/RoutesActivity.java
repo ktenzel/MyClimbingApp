aafa46df2465b8e2c9dc522b8e6e7c2651664cb7
kyle tenzeldam <ktenzel@gmail.com>
2018-03-16 21:35:20 -0700

    add title to routes activity and typeface

--- app/src/main/java/com/epicodus/example/myclimbingapp/RoutesActivity.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/RoutesActivity.java
@@ -1,13 +1,25 @@
 package com.epicodus.example.myclimbingapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RoutesActivity extends AppCompatActivity {
+    @BindView(R.id.appMyRoutesTextView)
+    TextView mAppMyRoutesTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
+
+        ButterKnife.bind(this);
+        Typeface baseFont = Typeface.createFromAsset(getAssets(), "fonts/Base02.ttf");
+        mAppMyRoutesTextView.setTypeface(baseFont);
     }
 }
