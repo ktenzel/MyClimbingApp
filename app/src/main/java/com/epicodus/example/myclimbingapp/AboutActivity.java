334d56411e5fb859dd6c13dd278a98cee1d40f82
kyle tenzeldam <ktenzel@gmail.com>
2018-03-17 17:25:10 -0700

    fix but on about activity and add text from strings

--- app/src/main/java/com/epicodus/example/myclimbingapp/AboutActivity.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/AboutActivity.java
@@ -16,7 +16,7 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_routes);
+        setContentView(R.layout.activity_about);
 
         ButterKnife.bind(this);
         Typeface baseFont = Typeface.createFromAsset(getAssets(), "fonts/Base02.ttf");
