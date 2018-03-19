bbdbff595b35ec13f9153ef90a9df8e5ee9a9903
kyle tenzeldam <ktenzel@gmail.com>
2018-03-18 22:15:25 -0700

    add readme.md

--- app/src/main/java/com/epicodus/example/myclimbingapp/ContactActivity.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/ContactActivity.java
@@ -1,13 +1,23 @@
 package com.epicodus.example.myclimbingapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ContactActivity extends AppCompatActivity {
+    @BindView(R.id.appContactTextView)
+    TextView mContactNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Typeface baseFont = Typeface.createFromAsset(getAssets(), "fonts/Base02.ttf");
+        mContactNameTextView.setTypeface(baseFont);
     }
 }
