222797b6357ed9531321aa9f9df7afc59ad17502
kyle tenzeldam <ktenzel@gmail.com>
2018-03-23 09:44:55 -0700

    add tokens and base URLS to contrants

--- app/src/main/java/com/epicodus/example/myclimbingapp/Contrants.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/Contrants.java
@@ -4,5 +4,7 @@ package com.epicodus.example.myclimbingapp;
 
 public class Contrants {
 //    public static final String GOOGLE_TOKEN = BuildConfig.GOOGLE_TOKEN;
+        public static final String GOOGLE_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 //    public static final String MOUNTAIN_TOKEN = BuildConfig.MOUNTAIN_TOKEN;
+        public static final String MOUNTAIN_BASE_URL = "https://www.mountainproject.com/data/get-routes-for-lat-lon?";
 }
