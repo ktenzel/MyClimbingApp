f7dd627ce04cd0d267689e24e7d65ee6d645a0bf
kyle tenzeldam <ktenzel@gmail.com>
2018-04-06 15:12:08 -0700

    begin working on layout changes

--- app/src/main/java/com/epicodus/example/myclimbingapp/ui/SavedRoutesListActivity.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/ui/SavedRoutesListActivity.java
@@ -19,48 +19,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SavedRoutesListActivity extends AppCompatActivity {
-    private DatabaseReference mFindRouteReference;
-    private FirebaseRecyclerAdapter mFirebaseAdapter;
-
-    @BindView(R.id.recyclerView)
-    RecyclerView mRecyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_find_route);
-        ButterKnife.bind(this);
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = user.getUid();
-
-        mFindRouteReference = FirebaseDatabase
-                .getInstance()
-                .getReference(Constants.FIREBASE_CHILD_ROUTES)
-                .child(uid);
-
-        setUpFirebaseAdapter();
-    }
-
-    private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Route, FirebaseFindRouteViewHolder>
-                (Route.class, R.layout.find_route_list_item, FirebaseFindRouteViewHolder.class,
-                        mFindRouteReference) {
-
-            @Override
-            protected void populateViewHolder(FirebaseFindRouteViewHolder viewHolder,
-                                              Route model, int position) {
-                viewHolder.bindRoute(model);
-            }
-        };
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setAdapter(mFirebaseAdapter);
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mFirebaseAdapter.cleanup();
+        setContentView(R.layout.activity_saved_route_list);
     }
 }
