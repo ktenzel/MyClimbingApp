a30c5073ea3c7d776dc62e4ca7d677cbfa9da692
kyle tenzeldam <ktenzel@gmail.com>
2018-07-20 12:13:38 -0700

    installed new relic mobile for testing and metrics

--- app/src/main/java/com/epicodus/example/myclimbingapp/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/example/myclimbingapp/ui/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.newrelic.agent.android.NewRelic;
 import com.epicodus.example.myclimbingapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
